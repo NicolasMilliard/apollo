9f6df9b6a45a58353d9b5813632c8e361ab6ea6f
Nicolas Milliard <contact@nicolasmilliard.fr>
2023-03-21 15:34:11 +0100

    Configure resolvers

--- odyssey-lift-off-part2/server/src/resolvers.js
+++ odyssey-lift-off-part2/server/src/resolvers.js
@@ -2,7 +2,18 @@ const resolvers = {
   Query: {
     // returns an array of Tracks that will be use to populate
     // the homepage grid of our web client
-    tracksForHome: (parent, args, context, info) => {},
+    // parent: returned value of the resolver for this field's parent
+    // args: object that contains all GraphQL arguments
+    // contextValue: object shared accros all resolvers
+    // info: information about the operation's execution state
+    tracksForHome: (_, __, { dataSources }) => {
+      return dataSources.trackAPI.getTracksForHome();
+    },
+  },
+  Track: {
+    author: ({ authorId }, _, { dataSources }) => {
+      return dataSources.trackAPI.getAuthor(authorId);
+    },
   },
 };
 
